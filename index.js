2830b6df4340ec9fe3f8f78d51624326a38c0abb
3JoB <54469706+3JoB@users.noreply.github.com>
2021-05-03 21:06:58 +0800

    新增对BV的支持
    新增对BV的支持(大小写均可)
    我笨，只能这样改

--- index.js
+++ index.js
@@ -7,12 +7,40 @@
 				'<iframe allowfullscreen="" scrolling="no" src="//player.bilibili.com/player.html?aid=$1&as_wide=1" ' +
 				'style="border:0;height:100%;left:0;position:absolute;width:100%"></iframe></div>'
 		},
+		{ // Video
+			from: /<a href="(?:https?:\/\/)?(?:www\.)?bilibili\.(?:tv|com)\/video\/BV(\S+).*?">.+<\/a>/g,
+			to: '<div class="embed-responsive embed-responsive-16by9">' +
+				'<iframe allowfullscreen="" scrolling="no" src="//player.bilibili.com/player.html?bvid=$1&as_wide=1" ' +
+				'style="border:0;height:100%;left:0;position:absolute;width:100%"></iframe></div>'
+		},
+		{ // Video
+			from: /<a href="(?:https?:\/\/)?(?:www\.)?bilibili\.(?:tv|com)\/video\/bv(\S+).*?">.+<\/a>/g,
+			to: '<div class="embed-responsive embed-responsive-16by9">' +
+				'<iframe allowfullscreen="" scrolling="no" src="//player.bilibili.com/player.html?bvid=$1&as_wide=1" ' +
+				'style="border:0;height:100%;left:0;position:absolute;width:100%"></iframe></div>'
+		},
 		{
 			// b23 video (short url)
 			// eg: https://b23.tv/av*****
 			from: /<a href="(?:https?:\/\/)?(?:www\.)?b23\.(?:tv|com)\/av(\d+).*?">.+<\/a>/g,
 			to: '<div class="embed-responsive embed-responsive-16by9">' +
 				'<iframe allowfullscreen="" scrolling="no" src="//player.bilibili.com/player.html?aid=$1&as_wide=1" ' +
+				'style="border:0;height:100%;left:0;position:absolute;width:100%"></iframe></div>'
+		},
+		{
+			// b23 video (short url)
+			// eg: https://b23.tv/bv*****
+			from: /<a href="(?:https?:\/\/)?(?:www\.)?b23\.(?:tv|com)\/BV(\S+).*">.+<\/a>/g,
+			to: '<div class="embed-responsive embed-responsive-16by9">' +
+				'<iframe allowfullscreen="" scrolling="no" src="//player.bilibili.com/player.html?bvid=$1&as_wide=1" ' +
+				'style="border:0;height:100%;left:0;position:absolute;width:100%"></iframe></div>'
+		},
+		{
+			// b23 video (short url)
+			// eg: https://b23.tv/bv*****
+			from: /<a href="(?:https?:\/\/)?(?:www\.)?b23\.(?:tv|com)\/bv(\S+).*">.+<\/a>/g,
+			to: '<div class="embed-responsive embed-responsive-16by9">' +
+				'<iframe allowfullscreen="" scrolling="no" src="//player.bilibili.com/player.html?bvid=$1&as_wide=1" ' +
 				'style="border:0;height:100%;left:0;position:absolute;width:100%"></iframe></div>'
 		}
 	];
