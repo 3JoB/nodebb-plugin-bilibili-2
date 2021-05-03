4e4fe6121f7a06a8d981ea7e064f60bddc73a575
3JoB <54469706+3JoB@users.noreply.github.com>
2021-05-03 21:08:02 +0800

    新增BV兼容
    新增BV兼容

--- public/src/bilibili.js
+++ public/src/bilibili.js
@@ -1,5 +1,5 @@
 //gen html
-function genBilibiliFrame(aid,cid,width,height,divid)
+function genBilibiliFramea(aid,cid,width,height,divid)
 {
 	var f = '<iframe src="https://player.bilibili.com/player.html?aid='+
 		aid+
@@ -17,15 +17,47 @@ function genBilibiliFrame(aid,cid,width,height,divid)
 	return f;
 }
 
+function genBilibiliFramebv(bvid,cid,width,height,divid)
+{
+	var f = '<iframe src="https://player.bilibili.com/player.html?bvid='+
+		bvid+
+		'&cid='+
+		cid+
+		'" width="'+
+		width+
+		'" height="'+
+		height+
+		'" frameborder="0" ' +
+		'webkitallowfullscreen ' +
+		'mozallowfullscreen ' +
+		'allowfullscreen>' +
+		'</iframe>'
+	return f;
+}
+
+
 //get video cid
-function genBiliVideo(aid,width,height,divid)
+function genBiliVideoa(aid,width,height,divid)
 {
 	$.get( "https://cors-anywhere.herokuapp.com/www.bilibili.com/video/av" + aid, function( data ) {
 		let d = data.match(/cid=\d+/)[0].match(/\d+/)[0];
 		console.log(d);
-		let frame = genBilibiliFrame(aid,d,width,height);
+		let frame = genBilibiliFramea(aid,d,width,height);
 		console.log(frame);
-		divid.className = "bilibilivideo";
+		divid.className = "bilibilivideoa";
+		divid.innerHTML = frame;
+	});
+}
+
+
+function genBiliVideobv(bvid,width,height,divid)
+{
+	$.get( "https://cors-anywhere.herokuapp.com/www.bilibili.com/video/bv" + aid, function( data ) {
+		let d = data.match(/cid=\d+/)[0].match(/\d+/)[0];
+		console.log(d);
+		let frame = genBilibiliFramebv(bvid,d,width,height);
+		console.log(frame);
+		divid.className = "bilibilivideobv";
 		divid.innerHTML = frame;
 	});
 }
